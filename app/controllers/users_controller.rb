dc36fa5e4a50346b7037320144d52ec97dedde4d
Tadea Simunovic <tsimunoviczg@gmail.com>
2018-10-20 21:39:48 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if logged_in?
+     redirect to '/tweets'
+   else
+     erb :'users/create_user'
+   end
+ end
 
+ post '/signup' do
+     if params[:username] == "" || params[:password] == "" || params[:email] == ""
+       redirect to '/signup'
+     else
+       @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+       @user.save
+       session[:user_id] = @user.id
+       redirect to '/tweets'
+     end
+ 	end
 end
